414359112d7480f2324eb6791ca60f01000d5779
volkovasystems <richeve.bebedor@gmail.com>
2017-04-10 11:20:14 +0800

    Finished eqe procedure.

---
+++ .install.sh
@@ -0,0 +1,20 @@
+yarn add \
+babel-runtime \
+nafe \
+protype \
+stringe \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+nafe \
+protype \
+stringe \
+--save \
+--force-latest
---
+++ eqe-test.js
@@ -0,0 +1,5 @@
+
+const eqe = require( "./eqe.js" );
+
+console.log( eqe( function(){}, function(){} ) );
+console.log( eqe( 123, 456 ) );
