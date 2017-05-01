cbdea4200e60c124abe41894524ae28760247683
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-01 16:26:26 +0800

    Updated test.

--- eqe-test.js
+++ eqe-test.js
@@ -1,5 +1,7 @@
-
+const assert = require( "assert" );
 const eqe = require( "./eqe.js" );
 
-console.log( eqe( function(){}, function(){} ) );
-console.log( eqe( 123, 456 ) );
+assert.equal( eqe( function(){}, function(){} ), true, "should be true" );
+assert.equal( eqe( 123, 456 ), false, "should be false" );
+
+console.log( "ok" );
