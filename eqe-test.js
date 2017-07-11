c66f2dd7dd2f1b002f4430ecf93032e711720c08
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-11 11:41:07 +0800

    Updated server-side test.

--- eqe-test.js
+++ eqe-test.js
@@ -2,21 +2,33 @@ const assert = require( "assert" );
 const eqe = require( "./eqe.js" );
 
 assert.equal( eqe( 0, 0 ), true, "should return true" );
-assert.equal( eqe( function(){}, function(){} ), true, "should be true" );
+
+assert.equal( eqe( function( ){ }, function( ){ } ), true, "should be true" );
+
 assert.equal( eqe( Array, Array ), true, "should return true" );
-assert.equal( eqe( Object, Object ), true, "should return true" );
-assert.equal( eqe( Date, Date ), true, "should return true" );
-assert.equal( eqe( RegExp, RegExp ), true, "should return true" );
+
+assert.equal( eqe( null, null ), true, "should be true" );
+
+assert.equal( eqe( Symbol.for( "hello" ), Symbol.for( "hello" ) ), true, "should be true" );
 
 assert.equal( eqe( 0, "0" ), false, "should return false" );
+
 assert.equal( eqe( 0, "" ), false, "should return false" );
-assert.equal( eqe( null, undefined ), false, "should return true" );
-assert.equal( eqe( { }, { } ), false, "should return false" );
+
+assert.equal( eqe( null, undefined ), false, "should return false" );
+
+assert.equal( eqe( { }, { } ), false, "should be false" );
+
+assert.equal( eqe( [ ], [ ] ), false, "should be false" );
+
+assert.equal( eqe( ( ) => { } ), false, "should be false" );
+
 assert.equal( eqe( Array, "Array" ), false, "should return false" );
-assert.equal( eqe( Array, Object ), false, "should return false" );
-assert.equal( eqe( 123, "123" ), false, "should return false" );
-assert.equal( eqe( new Date( ), Date ), false, "should return false" );
+
 assert.equal( eqe( function hello( ){ return "hello" }, function hello( ){ return "hi" } ), false, "should return false" );
+
 assert.equal( eqe( 123, 456 ), false, "should be false" );
+
+assert.equal( eqe( Symbol( "hi" ), Symbol( "hi" ) ), false, "should be false" );
 
 console.log( "ok" );
