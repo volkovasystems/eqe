a0010c34feb9eaa2988a025578c9e2b8b35660d7
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-24 16:52:26 +0800

    Updated test.

--- eqe-test.js
+++ eqe-test.js
@@ -3,13 +3,17 @@ const eqe = require( "./eqe.js" );
 
 assert.equal( eqe( 0, 0 ), true, "should return true" );
 
-assert.equal( eqe( function( ){ }, function( ){ } ), true, "should be true" );
+assert.equal( eqe( function( ){ }, function( ){ } ), true, "should return true" );
 
 assert.equal( eqe( Array, Array ), true, "should return true" );
 
-assert.equal( eqe( null, null ), true, "should be true" );
+assert.equal( eqe( null, null ), true, "should return true" );
 
-assert.equal( eqe( Symbol.for( "hello" ), Symbol.for( "hello" ) ), true, "should be true" );
+assert.equal( eqe( undefined, undefined ), true, "should return true" );
+
+assert.equal( eqe( Symbol.for( "hello" ), Symbol.for( "hello" ) ), true, "should return true" );
+
+assert.equal( eqe( Symbol( "hi" ).toString( ), Symbol( "hi" ).toString( ) ), true, "should return true" );
 
 assert.equal( eqe( 0, "0" ), false, "should return false" );
 
@@ -17,18 +21,16 @@ assert.equal( eqe( 0, "" ), false, "should return false" );
 
 assert.equal( eqe( null, undefined ), false, "should return false" );
 
-assert.equal( eqe( { }, { } ), false, "should be false" );
+assert.equal( eqe( { }, { } ), false, "should return false" );
 
-assert.equal( eqe( [ ], [ ] ), false, "should be false" );
+assert.equal( eqe( [ ], [ ] ), false, "should return false" );
 
-assert.equal( eqe( ( ) => { } ), false, "should be false" );
+assert.equal( eqe( ( ) => { }, ( ) => { } ), true, "should return true" );
 
 assert.equal( eqe( Array, "Array" ), false, "should return false" );
 
 assert.equal( eqe( function hello( ){ return "hello" }, function hello( ){ return "hi" } ), false, "should return false" );
 
-assert.equal( eqe( 123, 456 ), false, "should be false" );
-
-assert.equal( eqe( Symbol( "hi" ), Symbol( "hi" ) ), false, "should be false" );
+assert.equal( eqe( 123, 456 ), false, "should return false" );
 
 console.log( "ok" );
