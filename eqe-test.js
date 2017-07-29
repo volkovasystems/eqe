f3fbcf019e491d322fdaaf54527a96793bf72c03
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-29 20:09:15 +0800

    Save changes.

--- eqe-test.js
+++ eqe-test.js
@@ -1,36 +1,70 @@
-const assert = require( "assert" );
+"use strict";
+
+/*;
+	@test-license:
+		The MIT License (MIT)
+		@mit-license
+
+		Copyright (@c) 2017 Richeve Siodina Bebedor
+		@email: richeve.bebedor@gmail.com
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-test-license
+
+	@test-configuration:
+		{
+			"package": "eqe",
+			"path": "eqe/test.module.js",
+			"file": "test.module.js",
+			"module": "test",
+			"author": "Richeve S. Bebedor",
+			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
+			"repository": "https://github.com/volkovasystems/eqe.git"
+		}
+	@end-test-configuration
+
+	@test-documentation:
+
+	@end-test-documentation
+
+	@include:
+		{
+			"assert": "should",
+			"eqe": "eqe"
+		}
+	@end-include
+*/
+
+const assert = require( "should" );
+
+//: @server:
 const eqe = require( "./eqe.js" );
+//: @end-server
 
-assert.equal( eqe( 0, 0 ), true, "should return true" );
+//: @client:
+const eqe = require( "./eqe.support.js" );
+//: @end-client
 
-assert.equal( eqe( function( ){ }, function( ){ } ), true, "should return true" );
-
-assert.equal( eqe( Array, Array ), true, "should return true" );
-
-assert.equal( eqe( null, null ), true, "should return true" );
-
-assert.equal( eqe( undefined, undefined ), true, "should return true" );
-
-assert.equal( eqe( Symbol.for( "hello" ), Symbol.for( "hello" ) ), true, "should return true" );
-
-assert.equal( eqe( Symbol( "hi" ).toString( ), Symbol( "hi" ).toString( ) ), true, "should return true" );
-
-assert.equal( eqe( 0, "0" ), false, "should return false" );
-
-assert.equal( eqe( 0, "" ), false, "should return false" );
-
-assert.equal( eqe( null, undefined ), false, "should return false" );
-
-assert.equal( eqe( { }, { } ), false, "should return false" );
-
-assert.equal( eqe( [ ], [ ] ), false, "should return false" );
-
-assert.equal( eqe( ( ) => { }, ( ) => { } ), true, "should return true" );
-
-assert.equal( eqe( Array, "Array" ), false, "should return false" );
-
-assert.equal( eqe( function hello( ){ return "hello" }, function hello( ){ return "hi" } ), false, "should return false" );
-
-assert.equal( eqe( 123, 456 ), false, "should return false" );
-
-console.log( "ok" );
+//: @bridge:
+const path = require( "path" );
+//: @end-bridge
